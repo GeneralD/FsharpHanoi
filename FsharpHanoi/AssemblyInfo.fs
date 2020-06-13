ba47c2e6100730fb33f656658fd45884df3f2b4c
GeneralD <yumejustice@gmail.com>
2020-06-13 13:00:37 +0900

    first commit

---
+++ FsharpHanoi/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿namespace FsharpHanoi
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("FsharpHanoi")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("ZYXW")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ FsharpHanoi/Program.fs
@@ -0,0 +1,34 @@
+﻿open FSharpPlus
+open FSharpPlus.Data
+
+let ($) a b = a b
+
+type Step<'T> =
+    { Src : 'T
+      Dst : 'T
+      Height : int }
+
+let cons x y = x :: y
+let push x = modify $ cons x
+
+let rec hanoi src bare dst (height : int) =
+    match height with
+    | 0 -> State $ fun s -> ((), s) // return ()
+    | _ ->
+        monad {
+            do! hanoi src dst bare $ height - 1
+            do! push $ { Src = src
+                         Dst = dst
+                         Height = height }
+            do! hanoi bare src dst $ height - 1
+        }
+
+[<EntryPoint>]
+let main argv =
+    let result =
+        monad {
+            do! hanoi "Left" "Mid" "Right" 3
+            do! modify rev
+        }
+    printfn "%A" $ State.exec result []
+    0
